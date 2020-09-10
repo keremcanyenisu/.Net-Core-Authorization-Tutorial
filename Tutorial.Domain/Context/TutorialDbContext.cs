e2221f7be450d6b4c82e41c47fbe09fd077551f3
keremcanyenisu <cankeremyenisu@gmail.com>
2020-09-10 15:11:03 +0100

    Entity Framework Core

---
+++ Tutorial.Domain/Context/TutorialDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Tutorial.Domain.Entities;
+
+namespace Tutorial.Domain.Context
+{
+    public class TutorialDbContext : DbContext
+    {
+
+        public TutorialDbContext(DbContextOptions<TutorialDbContext> options)
+    : base(options)
+        { }
+
+        public DbSet<User> Users { get; set; }
+
+
+    }
+}
--- Tutorial/Startup.cs
+++ Tutorial/Startup.cs
@@ -7,25 +7,37 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
 using Tutorial.Business.Services;
+using Tutorial.Domain.Context;
 using Tutorial.Domain.Services;
 using Tutorial.Services;
 using Tutorial.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace Tutorial
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var connectionSring = Configuration.GetConnectionString("TutorialDatabase");
             services.AddControllers();
             services.AddScoped<IUserService, UserService>();
             services.AddTransient<CallerService>();
             services.AddSingleton<SingletonService>();
             services.AddScoped<ScopedService>();
             services.AddTransient<TransientService>();
+            services.AddDbContext<TutorialDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("TutorialDatabase")));
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,7 +48,7 @@ namespace Tutorial
                 app.UseDeveloperExceptionPage();
             }
 
-            
+
 
             app.UseRouting();
 
